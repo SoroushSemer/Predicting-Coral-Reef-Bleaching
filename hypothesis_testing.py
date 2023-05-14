a5001635006a446b1fe76fbdbc190fa8247c81a0
Soroush Semerkant <36822848+SoroushSemer@users.noreply.github.com>
2023-05-14 17:30:37 -0400

    cleaned up comments

--- hypothesis_testing.py
+++ hypothesis_testing.py
@@ -45,6 +45,8 @@ col_names = ['site', 'year', 'depth', 'coral_cover', 'jan', 'feb', 'mar', 'apr',
 #split the rdd into csv format
 rdd = rddA.map(lambda row: row.split(','))
 
+
+#converts all temperature values to flosts
 def convert_to_float(row):
     row[2] = float(row[2])
     for i in range(4, len(row)):
@@ -52,6 +54,8 @@ def convert_to_float(row):
     return row
 rdd = rdd.map(convert_to_float)
 
+
+#converts the coral cover rankings to percentages
 def convert_to_coral_cover_percent(row):
 
     bleach_conversions = {
@@ -72,15 +76,19 @@ def convert_to_coral_cover_percent(row):
 
 rdd = rdd.map(convert_to_coral_cover_percent)
 
+#pulls out only the temperature coral cover and depth data
 only_data = rdd.map(lambda row:  row[2:])
 
+#transposes the rdd
 transposed_rdd = only_data.flatMap(lambda row: [(i, str(v)) for i,v in enumerate(row)])\
                     .reduceByKey(lambda a,b: a+","+b )\
                     .map(lambda row: (col_names[row[0]+2], np.asarray([float(i) for i in row[1].split(',')])))
 
+#filters out the coral cover row
 coral_cover = transposed_rdd.filter(lambda row: row[0] == 'coral_cover').collect()[0][1]
 coral_cover = sc.broadcast(coral_cover)
 
+#calculates the pearson product moment correlation
 def pearson(x, y):
     xmean = np.mean(x)
     ymean = np.mean(y)
@@ -95,9 +103,13 @@ def pearson(x, y):
 
 pearson_corr = transposed_rdd.map(lambda row: (row[0], pearson(row[1], coral_cover.value), row[1]))
 
+#gets degrees of freedom
 n = rdd.count()
 df = n - 2
 df = sc.broadcast(df)
+m = transposed_rdd.count()
+m = sc.broadcast(m)
+#gets p value from pearson correlation
 def p_val(pearson_corr):
 
     sqrt_df = math.sqrt(df.value)
@@ -106,13 +118,13 @@ def p_val(pearson_corr):
     t_val = t_val_numer / t_val_denom
 
     p_val = 2* tdist.sf(abs(t_val), df.value)
-    return p_val
+    return p_val / m.value
 
 associations = pearson_corr.map(lambda row: (row[0], row[1], p_val(row[1]), row[2]))
 
 print(associations.first())
 
-
+#formats and prints data nicely
 associations = associations.filter(lambda row: row[0] != 'coral_cover').sortBy(lambda row: abs(row[1]))
 associations = associations.collect()
 
@@ -143,6 +155,7 @@ col_names = rdd.first()
 #filter out the header
 rdd = rdd.filter(lambda row: row != col_names)
 
+#caculate the sample mean of the temperatures
 def get_mean_n(row):
     start = col_names.index('1997-01')
     end = col_names.index('2021-12')
@@ -159,6 +172,7 @@ def get_mean_n(row):
     return row
 rdd_with_mean_n = rdd.map(get_mean_n)
 
+#calculate the sample standard deviation
 def get_stdev(row):
     start = col_names.index('1997-01')
     end = col_names.index('2021-12')
@@ -174,20 +188,10 @@ def get_stdev(row):
 
 rdd_with_mean_n_stdev = rdd_with_mean_n.map(get_stdev)
 
-# bleach_thresholds = {
-#     1: 30.0,
-#     2: 27.7,
-#     3: 27.5,
-#     4: 28.8,
-#     5: 0.40,
-#     6: 0.50,
-#     7: 0.625,
-#     8: 0.75,
-#     9: 0.875,
-# }
-
+# bleach threshold value from research
 bleach_threshold = 28.6111
 
+#calculates the t value against the bleach threshold
 def get_t_val(row):
     t = (row[-3] - bleach_threshold) / (row[-1] / math.sqrt(row[-2]))
     row.append(t)
@@ -195,6 +199,7 @@ def get_t_val(row):
 
 rdd_with_t = rdd_with_mean_n_stdev.map(get_t_val)
 
+#gets the p value from the t value using one tailed t-test
 def get_p_val(row):
     p = tdist.sf(row[-1], row[-3])
     row.append(p)
@@ -202,6 +207,7 @@ def get_p_val(row):
 
 rdd_with_p = rdd_with_t.map(get_p_val)
 
+#formats data
 results = rdd_with_p.sortBy(lambda row: abs(row[-1]))\
                     .map(lambda row: [row[0],row[col_names.index('REEF_NAME')],row[col_names.index('depth')],str(row[-1])])
 
@@ -227,17 +233,18 @@ def convert_bleach_thresholds(row):
         if(row[i] != ''):
             row[i] = bleach_conversions[row[i][:2]]
     return row
-
+#filters pvalues below 0.05 for significant reefs
 at_danger = rdd_with_p.filter(lambda row: row[-1]<0.05)\
                         .map(convert_bleach_thresholds)\
                         .map(lambda row: [row[0],row[col_names.index('REEF_NAME')],row[col_names.index('depth')],[float(row[i]) for i in range(col_names.index('1997-01'), col_names.index('2021-12')+1)], row[col_names.index('1997'):col_names.index('2021')+1]])\
                         .collect()
 
-
+#gets x axis for plotting
 x = col_names[col_names.index('1997-01'):col_names.index('2021-12')+1]
 y = [bleach_threshold for i in range(len(x))]
 plt.plot(x,y, label='Bleaching Threshold', linestyle='--')
 
+#plots significant reefs
 for row in at_danger:
     plt.plot(x, row[3], label=row[0]+" "+row[1] +" @"+str(round(float(row[2]),1))+"m")
 
@@ -257,5 +264,6 @@ plt.xlabel('Year')
 plt.legend()
 plt.show()
 
+#prints data nicely
 print("Reefs significantly above bleach threshold")
 print(tabulate(results, headers=['Site','Reef','Depth','p-value']))
